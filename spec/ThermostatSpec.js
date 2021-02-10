547984e1203d5d7045ae9e42cb23947dc43d750c
acavalla <anna.cavalla@gmail.com>
2021-02-10 16:27:08 +0000

    wrote down function and added minimum temp

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -16,4 +16,20 @@ describe('Thermostat', function() {
       expect(thermostat.temp()).toBe(21);
     });
   });
+
+  describe('down',function(){
+    it('can decrease the temperature', function(){
+      thermostat.down();
+      expect(thermostat.temp()).toBe(19);
+    });
+  });
+
+  describe('minimum temp',function(){
+    it('cannot go below 10', function(){
+      thermostat = new Thermostat(10);
+      expect( function() {
+        thermostat.down();
+      }).toThrow(new Error("Too cold"));
+    });
+  });
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,6 +1,7 @@
 class Thermostat {
-  constructor() {
-    this.tempy = 20;
+
+  constructor(setTemp = 20) {
+    this.tempy = setTemp;
   }
 
   temp() {
@@ -8,6 +9,17 @@ class Thermostat {
   }
 
   up() {
-    return this.tempy += 1;
+    this.tempy += 1;
   }
+
+  down() {
+    const minimumTemp = 10;
+    if(this.tempy > minimumTemp){
+      this.tempy -= 1;
+    } else {
+      throw new Error ('Too cold')
+    }
+  }
+
+
 }
