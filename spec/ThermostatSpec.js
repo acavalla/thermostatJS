aa81224c918c1be42a52a65cf69bfb32c0632228
Anna Cavalla <anna.cavalla@gmail.com>
2021-02-11 14:58:50 +0000

    business logic complete

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,4 +1,8 @@
+'use strict';
+
 describe('Thermostat', function() {
+  
+  var thermostat;
 
   beforeEach(function() {
     thermostat = new Thermostat();
@@ -30,6 +34,32 @@ describe('Thermostat', function() {
       expect( function() {
         thermostat.down();
       }).toThrow(new Error("Too cold"));
+    });
+  });
+
+  describe('power saving mode',function(){
+    it('cannot go above 25', function(){
+      thermostat = new Thermostat(25);
+      expect( function() {
+        thermostat.up();
+      }).toThrow(new Error("Too hot"));
+    });
+  });
+
+  describe('reset',function(){
+    it('resets to 20', function(){
+      thermostat.reset();
+      expect(thermostat.temp()).toBe(20)
+    });
+  });
+
+  describe('current energy usage',function(){
+    it('outputs energy status', function(){
+      expect(thermostat.energyUsage()).toBe("medium-usage")
+      thermostat = new Thermostat(10);
+      expect(thermostat.energyUsage()).toBe("low-usage")
+      thermostat = new Thermostat(26);
+      expect(thermostat.energyUsage()).toBe("high-usage")
     });
   });
 });
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -1,20 +1,48 @@
+'use strict';
+
 class Thermostat {
 
   constructor(setTemp = 20) {
     this.tempy = setTemp;
+    this.powerSave = true
+    this.MINIMUM_TEMP = 10
+    this.MAXIMUM_TEMP = 25
   }
 
   temp() {
     return this.tempy;
   }
 
-  up() {
-    this.tempy += 1;
+  reset() {
+    this.tempy = 20
   }
 
+  up() {
+    if(this.tempy < this.MAXIMUM_TEMP) {
+      this.tempy += 1;
+    } else {
+      throw new Error ('Too hot')
+    }
+  }
+
+  switchSaveOff() {
+    this.powerSave = false
+    this.MAXIMUM_TEMP = 32
+  }
+
+  energyUsage() {
+    if(this.tempy < 18){
+      return "low-usage";
+    } else if(this.tempy <= 25) {
+      return "medium-usage";
+    } else {
+      return "high-usage";
+    }
+  }
+
+
   down() {
-    const minimumTemp = 10;
-    if(this.tempy > minimumTemp){
+    if(this.tempy > this.MINIMUM_TEMP){
       this.tempy -= 1;
     } else {
       throw new Error ('Too cold')
